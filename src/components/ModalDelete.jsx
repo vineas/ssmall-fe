c321a253e07ab21c3c4d93e8450b4422a1843d93
vineas <alvienasyandika@gmail.com>
2024-01-24 11:53:39 +0700

    add update & delete

--- src/components/ModalDelete.jsx
+++ src/components/ModalDelete.jsx
@@ -58,7 +58,7 @@ const ModalDelete = ({product_id}) => {
                             </div>
                         </div>
                     </div>
-                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div> 
                 </>
             ) : null}
         </>
